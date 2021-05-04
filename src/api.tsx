8328372f00995ddcec3f3fc4ed64a0dd30aad902
Mike Singleton <mike@pickaxe.nyc>
2021-05-04 13:16:41 -0400

    Allow Iterate.reset() to be called before Init()

--- src/api.tsx
+++ src/api.tsx
@@ -2,10 +2,10 @@ import type { ApiResponse, EmbedContext, EmbedResults, Survey } from './types';
 import { DefaultHost } from './constants';
 
 class ApiClient {
-  apiKey: string;
+  apiKey?: string;
   apiHost: string;
 
-  constructor(apiKey: string, apiHost: string = DefaultHost) {
+  constructor(apiKey?: string, apiHost: string = DefaultHost) {
     this.apiKey = apiKey;
     this.apiHost = apiHost;
   }
@@ -31,6 +31,10 @@ class ApiClient {
   };
 
   post = <T,>(path: string, body: {} = {}): Promise<ApiResponse<T>> => {
+    if (this.apiKey == null) {
+      throw 'Error sending event to Iterate: missing api key. Make sure you call Iterate.init() before calling any other methods, see README for details';
+    }
+
     return fetch(`${this.apiHost}/api/v1${path}`, {
       method: 'POST',
       headers: {
