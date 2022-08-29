b7e9b3b268469df20fe2653e9518314b7c087cff
Matt Healy <matt@pickaxe.nyc>
2022-08-29 16:10:30 -0500

    fix: unused import

--- example/android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ example/android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -2,8 +2,6 @@ package com.awesomeproject;
 
 import android.app.Application;
 import android.content.Context;
-import android.webkit.WebView;
-
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
