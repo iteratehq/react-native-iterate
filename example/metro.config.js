95ca0a98166d99192a6cdbcf66af609aa77d3541
Mike Singleton <mike@iteratehq.com>
2023-11-18 13:52:17 -0500

    Implement with new Bob version

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,25 +1,36 @@
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
+const { getDefaultConfig } = require('@expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
+const modules = Object.keys({ ...pak.peerDependencies });
 
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = {
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  ...defaultConfig,
+
   projectRoot: __dirname,
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
+  // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
+    ...defaultConfig.resolver,
+
+    blacklistRE: exclusionList(
       modules.map(
-        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-      ),
+        (m) =>
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
@@ -27,13 +38,6 @@ module.exports = {
       return acc;
     }, {}),
   },
-
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
 };
+
+module.exports = config;
